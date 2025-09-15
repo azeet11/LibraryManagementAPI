93df2a5a61c8b86c72a6e295022174b965498fe9
Ajit Nakarmi <ajit.nakarmi@amniltech.com>
2025-09-15 12:37:01 +0545

    Add project files.

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementSystem.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public bool IsBorrowed { get; set; } = false;
+}
---
+++ LibraryManagementSystem/Models/Loan.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementSystem.Models;
+
+public class Loan
+{
+    public int BookId { get; set; }
+    public int MemberId { get; set; }
+    public DateTime BorrowedDate { get; set; } = DateTime.Now;
+    public DateTime? ReturnedDate { get; set; }
+}
